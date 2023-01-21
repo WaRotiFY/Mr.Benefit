f34ae8cc06b1798eb0df28ece949e06f852b827d
shadowshaurma <123019033+WaRotiFY@users.noreply.github.com>
2023-01-21 17:50:36 +0300

    Update class.py

--- main/class.py
+++ main/class.py
@@ -1,15 +1,23 @@
+import sqlite3
+
+conn = sqlite3.connect("server.db")
+cur = conn.cursor()
+
 #класс профиля пользователя
 class User:
     def __init__(self, name=0, money=0):#заношу личные данные
         self.name = name
         self.money = money
-    def getmoney(self):#
+    def getmoney(self):#получаю значение баланса
         return self.money
-    def setmoney(self, set):
+    def setmoney(self, set):#изменяем баланс пользователя
         self.money = self.money+set
 
-
+#класс для работы с достижениями
 class Achievement:
-    def __init__(self, tasks=[-1], ismade=[-1]):
+    def __init__(self, tasks=[-1], ismade=[-1]):#заношу все данные о достижениях
         self.tasks = dict(zip(tasks,ismade))
+    def addAch(self, task):#добавить достижение в базу данных
+        cur.execute("""INSERT INTO achievement VALUES(?,?);""", (task,0))
+        conn.commit()
 
