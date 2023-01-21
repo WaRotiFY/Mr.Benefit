620ff04f8de4d963f8e3a4a006b116d17960dd18
shadowshaurma <123019033+WaRotiFY@users.noreply.github.com>
2023-01-21 16:47:44 +0300

    Update base.py

--- main/base.py
+++ main/base.py
@@ -2,11 +2,13 @@ import sqlite3
 
 conn = sqlite3.connect("server.db")
 cur = conn.cursor()
+#Создаю базу данных с профилем пользователя
 cur.execute("""CREATE TABLE IF NOT EXISTS user(
             TEXT name,
             INTEGER money);
             """)
 conn.commit()
+#Создаю базу данных с профилем пользователя
 cur.execute("""CREATE TABLE IF NOT EXISTS achievement(
             TEXT task,
             INTEGER ismade);
