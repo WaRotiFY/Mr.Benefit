6d50268880eaa503f9af6b89d20cd71db85331be
shadowshaurma <123019033+WaRotiFY@users.noreply.github.com>
2023-01-21 17:50:23 +0300

    Update base.py
    перестроил поля таблицы

--- main/base.py
+++ main/base.py
@@ -4,13 +4,13 @@ conn = sqlite3.connect("server.db")
 cur = conn.cursor()
 #Создаю базу данных с профилем пользователя
 cur.execute("""CREATE TABLE IF NOT EXISTS user(
-            TEXT name,
-            INTEGER money);
+            name TEXT,
+            money INTEGER);
             """)
 conn.commit()
 #Создаю базу данных с профилем пользователя
 cur.execute("""CREATE TABLE IF NOT EXISTS achievement(
-            TEXT task,
-            INTEGER ismade);
+            task TEXT,
+            ismade INTEGER);
             """)
 conn.commit()
