8ec9d7cb1c5eeec0d97633aa92c2bf5be15bea6e
shadowshaurma <123019033+WaRotiFY@users.noreply.github.com>
2023-01-21 17:51:17 +0300

    Update view.py
    файл главный для запуска

--- main/view.py
+++ main/view.py
@@ -3,11 +3,13 @@ from kivy.lang import Builder
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
+from Class import Achievement
 
-
+main=Achievement()
 class Container(GridLayout):
     def add_task(self):
-        pass
+        set=self.task.text
+        main.addAch(task=set)
 
 
 class MyApp(App):
