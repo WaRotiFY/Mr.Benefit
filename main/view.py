1156e573a2997492e2173b9bfb231aa7efc42c76
shadowshaurma <123019033+WaRotiFY@users.noreply.github.com>
2023-01-25 21:02:39 +0300

    Update view.py

--- main/view.py
+++ main/view.py
@@ -7,10 +7,13 @@ from kivy.lang import Builder
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.properties import ObjectProperty
+
 from Class import Achievement
 
 main=Achievement()
 class Container(GridLayout):
+    task = ObjectProperty
     def add_task(self):
         set=self.task.text
         main.addAch(task=set)
