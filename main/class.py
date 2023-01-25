3224c1df625e4b969052a8b91f23123a16f225ac
shadowshaurma <123019033+WaRotiFY@users.noreply.github.com>
2023-01-25 21:02:28 +0300

    Update class.py

--- main/class.py
+++ main/class.py
@@ -5,18 +5,40 @@ cur = conn.cursor()
 
 #класс профиля пользователя
 class User:
-    def __init__(self, name=0, money=0):#заношу личные данные
-        self.name = name
-        self.money = money
+    def __init__(self, name):#заношу личные данные
+        cur.execute("SELECT name FROM user WHERE name = ?", (name,))
+        if cur.fetchone() is None:
+            cur.execute("""INSERT INTO user VALUES(?,?);""", (name,0))
+            conn.commit()
+        profile = cur.execute(f"""SELECT * FROM user WHERE name="{name}"  """).fetchone()
+        self.name = profile[0]
+        self.money = profile[1]
     def getmoney(self):#получаю значение баланса
+        self.__init__(name=(self.name))
         return self.money
     def setmoney(self, set):#изменяем баланс пользователя
+        self.__init__(name=(self.name))
         self.money = self.money+set
+        sql_update_query = f"""UPDATE user SET money = {self.money} WHERE name = "{self.name}"  """
+        cur.execute(sql_update_query)
+        conn.commit()
+    def getname(self):#получаю имя пользователя
+        self.__init__(name=(self.name))
+        return self.name
 
 #класс для работы с достижениями
 class Achievement:
-    def __init__(self, tasks=[-1], ismade=[-1]):#заношу все данные о достижениях
-        self.tasks = dict(zip(tasks,ismade))
+    def __init__(self, name):#заношу все данные о достижениях
+        self.name=name
+        id = (cur.execute(f"""SELECT rowid FROM user WHERE name = "{name}" """)).fetchone()
+        tasks = (cur.execute(f"""SELECT task FROM achievement WHERE id={id[0]}""")).fetchall()
+        ismade = (cur.execute(f"""SELECT ismade FROM achievement WHERE id={id[0]}""")).fetchall()
+        self.id=id[0]
+        if tasks!=None:
+            self.tasks = dict(zip(tasks,ismade))
     def addAch(self, task):#добавить достижение в базу данных
-        cur.execute("""INSERT INTO achievement VALUES(?,?);""", (task,0))
+        cur.execute("""INSERT INTO achievement VALUES(?,?,?);""", (self.id, task,0))
         conn.commit()
+    def  getAch(self):
+        self.__init__(self.name)
+        return self.tasks
