e4176e951c9f71e9d64f4455ea7f7c1c54aa57df
shadowshaurma <123019033+WaRotiFY@users.noreply.github.com>
2023-02-03 21:55:43 +0300

    Update view.py

--- main/view.py
+++ main/view.py
@@ -4,12 +4,14 @@
 
 from kivy.animation import Animation
 from kivy.lang import Builder
-from kivy.properties import NumericProperty
+from kivy.properties import NumericProperty, ObjectProperty
+from kivy.uix.screenmanager import ScreenManager
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
+from Class import User
 
 
-class FirstUI(MDScreen):
+class StartWindow(MDScreen):
     animation_constant = NumericProperty(40)
 
     def __init__(self, **kw):
@@ -19,12 +21,19 @@ class FirstUI(MDScreen):
         anim.repeat = True
         anim.start(self)
 
+class SingIn(MDScreen):
 
-class App(MDApp):
+    def addNick(self):
+        print(self.ids.text_input.text)
+        person = User(self.ids.text_input.text)
+
+class WindowManage(ScreenManager):
+    pass
+class mainApp(MDApp):
 
     def build(self):
         return Builder.load_file('main.kv')
 
 
 if __name__ == '__main__':
-    App().run()
+    mainApp().run()
